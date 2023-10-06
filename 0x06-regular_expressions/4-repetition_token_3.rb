924076bba91c1c13a67fca79771735690891e57c
Yoitsjae <ncubejason896@gmail.com>
2023-10-06 16:49:50 -0700

    update

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,17 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if there is an argument provided
-if ARGV.empty?
-  puts "Usage: #{$0} <string>"
-  exit 1
-end
-
-# Get the first command line argument
-input_string = ARGV[0]
-
-# Check if the input string matches the repetition token pattern
-if input_string.match?(/hb[^t]*n/)
-  puts input_string
-else
-  puts "No match"
-end
+puts ARGV[0].scan(/hbt*n/).join
